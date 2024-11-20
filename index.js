ee73db54045def7968e4840713c1f055c4c7b4a2
bhanuprakash <bhanuprakashande9@gmail.com>
2024-11-20 08:00:41 +0530

    expirement1 commit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const app = express();
+const dotEnv = require('dotenv');
 const mongoose = require('mongoose');
 const vendorRoutes = require('./routes/vendorRoutes');
 const bodyParser = require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-const path = require('path');
 const cors = require('cors');
+const path = require('path')
 
-const dotEnv = require('dotenv');
-
-
+const app = express()
 
 const PORT = process.env.PORT || 4000;
 
@@ -21,16 +19,16 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected successfully!"))
     .catch((error) => console.log(error))
 
-    app.use(bodyParser.json());
-    app.use('/vendor', vendorRoutes);
-    app.use('/firm', firmRoutes)
-    app.use('/product', productRoutes);
-    app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.json());
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes)
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
 
-app.listen(PORT,()=>{
-console.log(`server started and running at ${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`server started and running at ${PORT}`);
+});
 
-app.use('/home', (req, res) => {
-    res.send("<h1> Welcome to SWIGGY");
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to SUBY");
 })
